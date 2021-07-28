c96da2d093ac9d9c274c3be29d7be8f2c51a4ca8
MinkyungOh <44351651+MinkyungOh@users.noreply.github.com>
2021-07-28 10:31:10 +0900

    Add files via upload

---
+++ precache-manifest.dc3b68f14ebcf6d1d46e9d15bad51ac3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcc7b0d43d6f6453b30",
+    "url": "/my-insta/css/app.81c0675c.css"
+  },
+  {
+    "revision": "8b64f97d675b31df0b479b4a9753f0d7",
+    "url": "/my-insta/images/buildings.jpg"
+  },
+  {
+    "revision": "3d64b78c85d7d83988cbd17fd010458e",
+    "url": "/my-insta/images/octopus.png"
+  },
+  {
+    "revision": "c10b0dd1f0970b6bd4ff07454ea6ef7b",
+    "url": "/my-insta/images/palmtrees.jpg"
+  },
+  {
+    "revision": "236ecb98949fa36f5a21cfa6164ec134",
+    "url": "/my-insta/images/yellow-wall.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/my-insta/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2280d9149f232f88018b9a3eb106fe10",
+    "url": "/my-insta/index.html"
+  },
+  {
+    "revision": "1fcc7b0d43d6f6453b30",
+    "url": "/my-insta/js/app.43ff030f.js"
+  },
+  {
+    "revision": "46105e244ae8b9203b1b",
+    "url": "/my-insta/js/chunk-vendors.28391fdc.js"
+  },
+  {
+    "revision": "f673bbbd27a97ab2c6395826edee371a",
+    "url": "/my-insta/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-insta/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/my-insta/precache-manifest.dc3b68f14ebcf6d1d46e9d15bad51ac3.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "my-instar"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
